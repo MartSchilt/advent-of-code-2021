16b39ec37bb1e1c4d98dec42db25ae15106e3c4f
MartSchilt <schilthuismart@gmail.com>
2021-12-03 15:08:36 +0100

    Day3 Part 1

--- advent-of-code-2021/advent_of_code_2021.py
+++ advent-of-code-2021/advent_of_code_2021.py
@@ -12,8 +12,7 @@ def day1():
                     count += 1
             pn = n
         print("Number of measurements larger than the previous measurement:", count)
-    print("Part 2")
-    with open('Day1/input.txt') as i:
+        print("Part 2")
         lines = i.readlines()
         j = 0
         ps = None
@@ -56,4 +55,46 @@ def day2():
         print("Part 2")
         print(X, "x", Y, "=", X*Y)
 
-day2()
+def day3():
+    with open('Day3/input.txt') as i:
+        gamma_list = []
+        epsilon_list = []
+        rowsize = 1000
+        columnsize = 12
+        most_common_bits = [[0] * columnsize for i in range(rowsize)]
+        one = 0
+        zero = 0
+        k = 0
+        for line in i:
+            print(line)
+            j = 0
+            for char in line:
+                if char != '\n':
+                    number = int(char)
+                    most_common_bits[k][j] = number
+                j += 1
+            k += 1    
+
+        for y in range(12):
+            for x in range(rowsize):
+                if most_common_bits[x][y] == 1:
+                    one += 1
+                elif most_common_bits[x][y] == 0:
+                    zero += 1
+            if one > zero:
+                gamma_list.append(1)
+                epsilon_list.append(0)
+            else:
+                gamma_list.append(0)
+                epsilon_list.append(1)
+            one = 0
+            zero = 0
+        numbers = [str(integer) for integer in gamma_list]
+        g_string = "".join(numbers)
+        gamma = int(g_string, 2)
+        numbers = [str(integer) for integer in epsilon_list]
+        e_string = "".join(numbers)
+        epsilon = int(e_string, 2)
+        print(gamma, "x", epsilon, "=", gamma*epsilon)
+
+day3()
